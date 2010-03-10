e36309202fec55e27227a56acd1ddbfa990efa25
Peter Collingridge <peter.collingridge@gmail.com>
2010-03-10 15:26:22 +0000

    changes to biochemistry

--- biochemistry.py
+++ biochemistry.py
@@ -1,21 +1,43 @@
 class Metabolite:
     def __init__(self, name):
         self.name = name
-	self.solutions = {}
+        self.solutions = {}
 
 class Reaction:
-    def __init__(self, substrates, products, forward_rate, reverse_rate):
-        self.chemicals = (substrates, products)
+    def __init__(self, substrate_names, product_names, forward_rate, reverse_rate):
+
+        self.substrates = []
+        self.products = []
         self.rates = (forward_rate, reverse_rate)
 
-class Solution:
+        for s in substrate_names:
+            self.substrates.append[all_metabolites[s]]
+
+        for p in product_names:
+            self.products.append[all_metabolites[p]]
+
+class Solution():
     def __init__(self, volume):
-	self.volume = volume
+        self.volume = volume
+        self.metabolites = {}
 
-ATP = Metabolite('ATP')
-ADP = Metabolite('ADP')
-Pi  = Metabolite('Phosphates')
-ATPase = Reaction([ATP], [ADP, Pi], 0.1, 0.0001)
+        for metabolite in all_metabolites:
+            metabolite.solution[self] = 0.0
 
-all_metabolites = [ATP, ADP, Pi]
+class Cell():
+    def __init__(self, volume):
+        self.volume = volume
+
+class Protein():
+    def __init__(self, amount):
+        self.amount = amount
+
+# Define all the metabolites and reactions that exist
+metabolites = ['ATP', 'ADP', 'Phosphates']
+all_metabolites = {}
+
+for m in metabolites:
+    all_metabolites[m] = Metabolite[m]
+
+ATPase = Reaction(['ATP', ['ADP', 'Phosphates'], 0.1, 0.0001)
 all_reactions = [ATPase]
