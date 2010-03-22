28266b425571e8f5141885520d8e7bd4f83f0a56
Peter Collingridge <peter.collingridge@gmail.com>
2010-03-22 16:34:52 +0000

    Altered styling of graph

--- graphDrawer.py
+++ graphDrawer.py
@@ -2,12 +2,12 @@ class Graph():
     def __init__ (self):
         self.series = {}
         self.variables = {}
-        self.border = (60, 30, 15, 20)
+        self.border = (40, 5, 75, 40)
         self.scaleX = 1.0
         self.scaleY = 1.0
         self.colours = ['#0060e5', '#001060', '#e52060', '#a00030', '#00c020', '#006010' ]
 
-        self.X_axis = Axis(400)
+        self.X_axis = Axis(360)
         self.Y_axis = Axis(200)
 
     def addSeries(self, name):
@@ -35,8 +35,8 @@ class Graph():
 
         self.X_axis.range = (0, max_X)
         self.Y_axis.range = (0, max_Y)
-        self.X_axis.tick_interval = self.X_axis.length / 5
-        self.Y_axis.tick_interval = 3
+        self.X_axis.tick_interval = self.X_axis.range[1] / 5
+        self.Y_axis.tick_interval = 10
 
         self.X_axis.drawX(self.svg, self.border[0], self.Y_axis.length + self.border[1], self.scaleX)
         self.Y_axis.drawY(self.svg, self.border[0], self.Y_axis.length + self.border[1], self.scaleY)
@@ -44,11 +44,7 @@ class Graph():
         for s in series:
             self.drawPlot(self.series[s])
 
-        x = self.border[0] + 10
-        for s in series:
-            self.drawLabel(self.series[s], x, 16)
-            x += 4 + self.X_axis.length / len(series)
-    
+        self.drawLabels()
         self.svg.write('</svg>')
 
     def initiliseSVG(self, name):
@@ -64,24 +60,40 @@ xmlns="http://www.w3.org/2000/svg" version="1.1">
 <style type="text/css" id="style_css_sheet">
 
 .axis {
+  fill-opacity: 0;
   stroke: black;
   stroke-width: 1;
-  stroke-opacity: 0.95;
-  fill-opacity: 0;
+  stroke-opacity: 1;
 }
 
 .axis_text {
-  fill: black;
-  fill-opacity: 0.95;
-  stroke-width: 0;  
   font-size: 12px;
+  fill: black;
+}
+
+.axis_label {
+  font-size: 16px;
+  fill: black;
 }
 
 .data {
   fill-opacity: 0;
+  stroke: #989898;
   stroke-opacity:0.8;
-  stroke-width: 2.5;
+  stroke-width: 3;
 }
+
+.label {
+  stroke-width: 0;  
+  font-size: 16px;
+  fill: black;
+  opacity: 0.4;
+}
+
+.label:hover {
+  opacity: 0.9;
+}
+
 </style>
 """)
 
@@ -89,9 +101,10 @@ xmlns="http://www.w3.org/2000/svg" version="1.1">
         dx = self.border[0]
         dy = self.border[1] + self.Y_axis.length
 
-        self.svg.write('<path class="data" visibility="hidden" stroke="%s" ' % series.colour)
+        self.svg.write('<path class="data" visibility="hidden" ')
+    #    self.svg.write('stroke="%s" ' % series.colour)
 
-        for n in range(len (series.data)-1):
+        for n in range(len(series.data)):
             x = n * self.scaleX + dx
             y = dy - series.data[n] * self.scaleY
 
@@ -105,8 +118,20 @@ xmlns="http://www.w3.org/2000/svg" version="1.1">
         self.svg.write('begin="label%d.mouseover" end="label%d.mouseout"></set>\n' % (series.number, series.number))
         self.svg.write('</path>\n')
 
-    def drawLabel(self, series, x, y):
-        self.svg.write('<text id="label%d" x="%d" y="%d" fill="%s">%s</text>\n' % (series.number, x, y, series.colour, series.label))
+    def drawLabels(self):
+        x = self.border[0] + self.X_axis.length + 10
+        y = self.border[1] + 20
+
+        series = self.series.keys()
+        series.sort()
+
+        for s in series:
+            self.svg.write('<text id="label%d" class="label" x="%d" y="%d" ' % (self.series[s].number, x, y))
+            self.svg.write('fill="%s">%s</text>\n' % (self.series[s].colour, self.series[s].label))
+            y += self.Y_axis.length / len(self.series)
+
+    def writeFinalValue(self, series):
+        pass
 
 class DataSeries():
     def __init__(self, label, number):
@@ -122,25 +147,26 @@ class Axis():
         self.tick_interval = 0.2
 
     def drawX(self, svg, x, y, dx):
-        svg.write(' <path class= "axis" d="M%d, %d L%d, %d" />\n' % (x, y, x+self.length, y))
+        svg.write(' <path class="axis" d="M%d, %d L%d, %d" />\n' % (x, y, x+self.length, y))
+        svg.write(' <text class="axis_label" x="%d" y="%d">Time</text>\n' % (x+self.length/2-20, y+40))
 
         label = self.range[0]
-        while label < self.range[1]:
-            svg.write(' <path class= "axis" d="M%d %d L%d %d" />\n' % (x, y-0.5, x, y+8))
+        while label <= self.range[1]:
+            svg.write(' <path class="axis" d="M%d %d L%d %d" />\n' % (x, y-0.5, x, y+8))
             labelX = x - len(str(label))*4
-            svg.write(' <text class= "axis_text" x="%d" y="%d" >%s</text>\n' % (labelX, y+20, label))
+            svg.write(' <text class="axis_text" x="%d" y="%d" >%s</text>\n' % (labelX, y+20, label))
 
             x += self.tick_interval * dx
             label += self.tick_interval
 
     def drawY(self, svg, x, y, dy):
-        svg.write(' <path class= "axis" d="M%d, %d L%d, %d" />\n' % (x, y, x, y-self.length))
+        svg.write(' <path class="axis" d="M%d, %d L%d, %d" />\n' % (x, y, x, y-self.length))
 
         label = self.range[0]
-        while label < self.range[1]:
-            svg.write(' <path class= "axis" d="M%d %d L%d %d" />\n' % (x-8, y, x+0.5, y))
+        while label <= self.range[1]:
+            svg.write(' <path class="axis" d="M%d %d L%d %d" />\n' % (x-8, y, x+0.5, y))
             labelX = x - 10 - len("%.1f" % label)*7
-            svg.write(' <text class= "axis_text" x="%d" y="%d" >%.1f</text>\n' % (labelX, y+4, label))
+            svg.write(' <text class="axis_text" x="%d" y="%d" >%.0f%%</text>\n' % (labelX, y+4, label))
 
             y -= self.tick_interval * dy
             label += self.tick_interval
