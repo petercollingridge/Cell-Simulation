a79cc7f46dfc2a1bc40e94f47dce440384f95c6d
Peter Collingridge <peter.collingridge@gmail.com>
2010-03-28 20:07:47 +0100

    Improved the way metabolites are added. Added a doubleCells function - all passed cells replicate.

--- evolveCell.py
+++ evolveCell.py
@@ -12,7 +12,7 @@ def mutateSequence(template):
     n = 0
 
     while n < len(template):
-        if random.random() < 0.995:
+        if random.random() < 0.998:
             seq += template[n]
         else:
             if random.random() < 0.75:
@@ -23,56 +23,83 @@ def mutateSequence(template):
 
     return seq
 
-offspring = [12,8,8,8,4,4,4,4,1,1,1,1,1,1,1,1]
+def addCell(seq):
+    solution.addCell(1000.0)
+    solution.cells[-1].addDefaultMetabolites()
+    solution.cells[-1].DNA = seq
+    solution.cells[-1].interpretDNA()
 
+offspring = [12,8,8,8,4,4,4,4,1,1,1,1,1,1,1,1]
 def breedCells(cells):
     daughter_DNA = []
 
-    for n in range(16):
+    for n in range(len(offpring)):
         for daughter in range(offspring[n]):
             DNA = mutateSequence(cells[n].DNA)
             daughter_DNA.append(DNA)
 
     for n in range(4):
-        parent = random.randint(0,63)
+        parent = random.randint(0,number_of_cells-1)
         DNA = mutateSequence(cells[parent].DNA)
         daughter_DNA.append(DNA)
 
     return daughter_DNA
 
-ancestral_DNA = 'BACBDDAABACBDDAABAAADDAABAADDDAABCABDDAABABDBCAA'
+def doubleCells(cells):
+    daughter_DNA = []
 
+    for n in range(number_of_cells/2):
+        for daughters in range(2):
+            DNA = mutateSequence(cells[n].DNA)
+            daughter_DNA.append(DNA)
+
+    return daughter_DNA
+
+def outputGeneration(solution):
+    solution.cells.sort(lambda x, y: cmp(y.metabolites['IH'].amount, x.metabolites['IH'].amount))
+
+    for m in solution.metabolites.keys():
+        outputFile.write('%s:%f, ' % (m, solution.metabolites[m].amount/solution.volume))
+    outputFile.write('\n')
+
+    outputFile.write('%s\t%f\n' % (solution.cells[0].DNA, solution.cells[0].metabolites['IH'].amount))
+    print "%d, %.4f" % (generation, solution.cells[0].metabolites['IH'].amount)
+
+#   Define metabolites in pool
+defaultMetabolites = virtualCell.defaultMetabolites
+defaultMetabolites['FK'] = 1.6 / 10.0
+defaultMetabolites['IL'] = 0.8 / 10.0
+defaultMetabolites['FG'] = 0.6 / 10.0
+defaultMetabolites['JG'] = 0.4 / 10.0
+
+#   Create generation 0
+number_of_cells = 128
+generation_time = 10
+ancestral_DNA = 'BACC-BACD-BABCBBAD-BAACBBBA-BAABBCAA-BCAC-BCAD-BCAABDBB'.replace('-', 'DDAA')
 daughter_DNA = []
-for n in range(64):
+
+for n in range(number_of_cells):
     DNA = mutateSequence(ancestral_DNA)
     DNA = addRandomSequence(DNA)
     daughter_DNA.append(DNA)
 
-outputFile = file('genome.txt','w')
+outputFile = file('genomes.txt','w')
 generation = 0
 
 while True:
-    solution = virtualCell.Solution(1000000.0)
-    solution.addDefaultMetabolites()
-    solution.metabolites['EL'].amount = solution.volume * 0.08
-    solution.metabolites['FK'].amount = solution.volume * 0.04
+    solution = virtualCell.Solution(2000000.0)
+    solution.setMetabolites(defaultMetabolites)
 
-    for n in range(64):
+    for n in range(number_of_cells):
         solution.addCell(1000.0)
-        solution.cells[n].addDefaultMetabolites()
+        solution.cells[n].setMetabolites('default')
         solution.cells[n].DNA = daughter_DNA[n]
         solution.cells[n].interpretDNA()
 
-    for t in range(1000):
+    for t in range(generation_time):
         for cell in solution.cells:
             cell.update()
 
-    solution.cells.sort(lambda x, y: cmp(y.metabolites['EH'].amount, x.metabolites['EH'].amount))
-
-    outputFile.write('%s\t%f\n' % (solution.cells[0].DNA, solution.cells[0].metabolites['EH'].amount))
-    print "%d, %.4f" % (generation, solution.cells[0].metabolites['EH'].amount)
-
-    daughter_DNA = breedCells(solution.cells)
+    outputGeneration(solution)
+    daughter_DNA = doubleCells(solution.cells)
     generation += 1
-
-
