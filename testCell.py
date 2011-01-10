c870a1f07936c768fa13391d34c720aa451e3f1f
Peter Collingridge <peter@diannao.(none)>
2011-01-10 20:48:25 +0000

    Simplified test case to work with new virtualCell module.

--- testCell.py
+++ testCell.py
@@ -1,65 +1,33 @@
 import virtualCell
-import graphDrawer
+
+solutionMetabolites = virtualCell.default_metabolites
+solutionMetabolites['FK'] = 1.6 / 10.0
+solutionMetabolites['IL'] = 0.8 / 10.0
+solutionMetabolites['FG'] = 0.6 / 10.0
+solutionMetabolites['JG'] = 0.4 / 10.0
+solutionMetabolites['EL'] = 0.1 / 10.0
 
 # Initilise Solution
-solution = virtualCell.Solution(1000000.0)
-#solution.volume /= 64
-solution.setMetabolites('default')
-solution.metabolites['EL'].amount = solution.volume * 0.08
-solution.metabolites['FK'].amount = solution.volume * 0.04
+solution = virtualCell.Solution(1000000.0/64)
+solution.setMetabolites(solutionMetabolites)
 
-DNA = {'Ancestor' : 'BACBDDAABACBDDAABAAADDAABAADDDAABCABDDAABABDBCAA',
-       'No E pore': 'BACBDDAABACBDDAABAADDDAABCABDDAABABDBCAA',
-       'E/H antiporter' : 'BACBDDAABACBDDAABAAABBADDDAABCABDDAABABDBCAA',
-       'Gen 1': 'BACBDDAABACBDDAABAAADDAABAADDDAABCABDDAABABDBCAADADADBDDCBACCCCAACC',
-       'Gen 2800': 'BACBCCDBCBCAADADDAABABDBBADDCCAAADADBACCBCADCCDDAABCABDDAABABDBCAACCACACABDCAADDAABABDBBADCBDDDAABACBCCDBCBCAADADDAABABDBBADDADDABADCABCBDDAABCABDDAABABDBCAADDCDCDDDDAABABDBBADBACDDAABACBCCDBCBCAADADDAABABDBBADDACDAADBAAAADDDAABCABDDAABABDBCAAAAACCDCABDDAABABDBCAAADDDDDABCCAACBADCDADDAABABDBBADCBCDDAABABDBBADDCDABBDDAABABDBCAAACACCDCCDCBAB',
-       'Gen 2739': 'BACBCCDBCBCAADADDAABABDBBADDCCAAADACDACCBCADCCDDAABCABDDAABABDBCAACCCCACABDCAADDAABABDBBADCBDDDAABACBCCDBCBCAADADDAABABDBBADDADDABADCABCBDDAABCABDDAABABDBCAADDCDCDDBCAADDCDDDDAABABDBBADBACDDAABACBCCDBCBCAADADDAABABDBBADDACDAADBAAAADDDAABCABDDAABABDBCAADAACCDCABDDAABABDBCAAADDADDABCCAAABADCDADDAABABDBBADCBCDDADDAABABDBBADBACDDAABACBCCDBCBCAADADDAABABDBBADDACDAADBAAAADDDAABCABDDAABABDBCAADAACCDCABDDAABABDBCAAADDDDDABCCAACBADCDADDAABABDBBADCBCDDAABABDBBADDCDABBDDAABABDBCAAACACCDCCDCBAB',
-'Gen2, 1263': 'BACBCBCCDBBDDAADBCCAABCABACCDCBDCBDDCADDAADADBABBABDBCAACDDDDAABABDBBADDDAABABDBBADDDAADADBABBABDBCAACDDDDAABABDBBADAD',
-'Gen 263': 'BACBDCBDDAABAADBBAACADACDDBDBBBAACDDCAADDDAABCABDDAABABDBCAACAAADCDCABACDCABBACDDAABABDBBADDBCDDAABABDBBADDBDADBABCBABDDAABABDBCAADADCAADBDBCBAADDDCABCCACCCAADBDADBDBCBAACCAADBA'}
+# Initilise Cell
+DNA = 'CABCAA-CABCAA-BACC-BACD-BADD-BAADBBBC-BAACBBBA-BCAC-BCAD-BAABBCAA-BCAABDBB'.replace('-', 'DDAA')
+solution.addCell(1000.0)
+solution.cells[-1].setMetabolites('default')
+solution.cells[-1].addDNA(DNA)
 
-def addCell(DNA_seq):
-    solution.addCell(1000.0)
-    solution.cells[-1].setMetabolites('default')
-    solution.cells[-1].DNA = DNA[DNA_seq]
-    solution.cells[-1].interpretDNA()
-    solution.cells[-1].outputProteins()
+for p in solution.cells[-1].proteins.values():
+    p.amount = 1
 
-#addCell('Gen 2800')
-addCell('Gen 263')
-
-# Initilise Graph
-graph = graphDrawer.Graph()
-graphSeries = { 'E 1': solution.cells[0].metabolites['E'],
-                'L 1': solution.cells[0].metabolites['L'],
-                'H 1': solution.cells[0].metabolites['H'],
-                'EL 1': solution.cells[0].metabolites['EL']}
-#graphSeries1 = {'E 2': solution.cells[1].metabolites['E'],
-#                'L 2': solution.cells[1].metabolites['L'],
-#                'H 2': solution.cells[1].metabolites['H'],
-#                'EL 2': solution.cells[1].metabolites['EL']}
-graphSeries2 = { 'E (out)': solution.metabolites['E'],
-                'L (out)': solution.metabolites['L'],
-                'H (out)': solution.metabolites['H'],
-                'EL (out)': solution.metabolites['EL']}
-
-series = graphSeries.keys()
-series.sort()
-for s in series:
-    graph.addSeries(s)
+print "\n-Proteins-"   
+solution.cells[-1].output('proteins')    
 
 # Run Simulation
-for t in range(20000):
-    for s in series:
-        graph.addDataToSeries(s, 100*graphSeries[s].amount/graphSeries[s].volume)
+solution.update(48000)
 
-    for cell in solution.cells:
-        cell.update()
-
+# Output
 print "\n-Solution-"
-solution.output()
-
-for cell in solution.cells:
-    print "\n-Cell-"
-    cell.output()
-
-#graph.outputSeries('top cell', series)
+solution.output('metabolites')
+print "\n-Cell-"
+solution.cells[-1].output('metabolites')
